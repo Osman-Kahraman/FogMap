6225e1f89763dc9a0dd2a7244d717927a6a4b2ce
Osman Kahraman <o_kahraman@outlook.com>
2026-03-14 02:32:09 -0400

    Initial Commit

---
+++ FogMap/FogMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FogMapApp.swift
+//  FogMap
+//
+//  Created by Osman Kahraman on 2026-03-14.
+//
+
+import SwiftUI
+
+@main
+struct FogMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
